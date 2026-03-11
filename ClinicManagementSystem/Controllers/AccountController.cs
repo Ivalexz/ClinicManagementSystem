a294a6f179d164e7fb701c18e5ab2166c397ae7a
Arsenipn <arsgog010309@gmail.com>
2026-03-11 19:51:13 +0200

    Account
    Додані форми Login та Register

---
+++ ClinicManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using ClinicManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementSystem.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+
+        if (!result.Succeeded)
+        {
+            ViewBag.Error = "Неправильний email або пароль";
+            return View();
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(string email, string password)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = email,
+            Email = email
+        };
+
+        var result = await _userManager.CreateAsync(user, password);
+
+        if (!result.Succeeded)
+        {
+            ViewBag.Error = "Помилка створення користувача";
+            return View();
+        }
+
+        await _signInManager.SignInAsync(user, false);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Login");
+    }
+}
